19bcc2f3a5a4a2d3a5b3ab811facadaebedaa329
Noah Lubko <nlubko@gmail.com>
2019-11-14 09:58:21 +0200

    add VideoListItem to components

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -5,8 +5,13 @@
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
+
 export default {
   name: "VideoList",
+  components: {
+    VideoListItem
+  },
   props: {
     videos: Array
   }
