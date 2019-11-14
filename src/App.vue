f104284b084eb2905573daab04ff1426ba20d3be
Noah Lubko <nlubko@gmail.com>
2019-11-14 07:26:38 +0200

    revert test

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
 <script>
 import axios from "axios";
 import SearchBar from "./components/SearchBar";
-
 import VideoList from "./components/VideoList";
 
 const API_KEY = "AIzaSyBeaVjeakVaXF-yzRDMc7tL_-d_rv_f-wA";
