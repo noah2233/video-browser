4d6670399b67cd7d4d5b8c4502438691118186dd
Noah Lubko <nlubko@gmail.com>
2019-11-15 12:48:02 +0200

    add style

--- src/components/VideoDetail.vue
+++ src/components/VideoDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="video">
+  <div v-if="video" class="col-md-8">
     <div class="embed-responsive embed-responsive-16by9">
       <iframe class="embed-responsive-item" :src="videoUrl" frameborder="0"></iframe>
     </div>
